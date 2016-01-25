bbccb790af54514e9e01bf3f8591485c77239931
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-01-25 17:30:32 +0100

    DBConnect

---
+++ OOPSA/OOPSA/Login.vb
@@ -0,0 +1,12 @@
+﻿Public Class Login
+    Private Sub Login_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.F10
+                Panel1.Visible = True
+        End Select
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Visible = False
+    End Sub
+End Class
