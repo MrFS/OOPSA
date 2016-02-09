d646cbbb0260b6cfdb5c8e115064ca1cb52f176a
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-02-09 07:05:58 +0100

    div

--- OOPSA/OOPSA/frmAdmin.Designer.vb
+++ OOPSA/OOPSA/frmAdmin.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmAdmin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,17 +20,24 @@ Partial Class frmAdmin
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        Me.dbNfo = New System.Windows.Forms.ListBox()
         Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.lblDBNfo = New System.Windows.Forms.Label()
+        Me.txtPort = New System.Windows.Forms.TextBox()
+        Me.txtDBUser = New System.Windows.Forms.TextBox()
+        Me.txtDB = New System.Windows.Forms.TextBox()
+        Me.txtServer = New System.Windows.Forms.TextBox()
         Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.txtDBpwd = New System.Windows.Forms.TextBox()
+        Me.RefreshToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChangeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -39,6 +46,7 @@ Partial Class frmAdmin
         CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer2.Panel1.SuspendLayout()
         Me.SplitContainer2.SuspendLayout()
+        Me.MenuStrip2.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -79,16 +87,6 @@ Partial Class frmAdmin
         Me.TabPage1.Text = "TabPage1"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(192, 74)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
         'SplitContainer1
         '
         Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
@@ -98,41 +96,117 @@ Partial Class frmAdmin
         'SplitContainer1.Panel1
         '
         Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.AutoScroll = True
         Me.SplitContainer1.Size = New System.Drawing.Size(1185, 549)
-        Me.SplitContainer1.SplitterDistance = 395
+        Me.SplitContainer1.SplitterDistance = 268
         Me.SplitContainer1.TabIndex = 0
-        '
-        'dbNfo
-        '
-        Me.dbNfo.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.dbNfo.FormattingEnabled = True
-        Me.dbNfo.Location = New System.Drawing.Point(0, 24)
-        Me.dbNfo.Name = "dbNfo"
-        Me.dbNfo.Size = New System.Drawing.Size(395, 181)
-        Me.dbNfo.TabIndex = 0
         '
         'SplitContainer2
         '
         Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
         Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
         Me.SplitContainer2.Name = "SplitContainer2"
         Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
         '
         'SplitContainer2.Panel1
         '
-        Me.SplitContainer2.Panel1.Controls.Add(Me.dbNfo)
+        Me.SplitContainer2.Panel1.AutoScroll = True
+        Me.SplitContainer2.Panel1.Controls.Add(Me.txtDBpwd)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.lblDBNfo)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.txtServer)
         Me.SplitContainer2.Panel1.Controls.Add(Me.MenuStrip2)
-        Me.SplitContainer2.Size = New System.Drawing.Size(395, 549)
-        Me.SplitContainer2.SplitterDistance = 205
+        Me.SplitContainer2.Panel1.Controls.Add(Me.txtPort)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.txtDBUser)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.txtDB)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.AutoScroll = True
+        Me.SplitContainer2.Size = New System.Drawing.Size(268, 549)
+        Me.SplitContainer2.SplitterDistance = 195
         Me.SplitContainer2.TabIndex = 0
+        '
+        'lblDBNfo
+        '
+        Me.lblDBNfo.AutoSize = True
+        Me.lblDBNfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDBNfo.Location = New System.Drawing.Point(5, 39)
+        Me.lblDBNfo.Name = "lblDBNfo"
+        Me.lblDBNfo.Size = New System.Drawing.Size(74, 144)
+        Me.lblDBNfo.TabIndex = 10
+        Me.lblDBNfo.Text = "Server:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Database:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Port:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Username:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Password:"
+        '
+        'txtPort
+        '
+        Me.txtPort.Location = New System.Drawing.Point(85, 100)
+        Me.txtPort.Name = "txtPort"
+        Me.txtPort.Size = New System.Drawing.Size(172, 20)
+        Me.txtPort.TabIndex = 9
+        '
+        'txtDBUser
+        '
+        Me.txtDBUser.Location = New System.Drawing.Point(85, 133)
+        Me.txtDBUser.Name = "txtDBUser"
+        Me.txtDBUser.Size = New System.Drawing.Size(172, 20)
+        Me.txtDBUser.TabIndex = 8
+        '
+        'txtDB
+        '
+        Me.txtDB.Location = New System.Drawing.Point(85, 68)
+        Me.txtDB.Name = "txtDB"
+        Me.txtDB.Size = New System.Drawing.Size(172, 20)
+        Me.txtDB.TabIndex = 7
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(85, 38)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(172, 20)
+        Me.txtServer.TabIndex = 6
         '
         'MenuStrip2
         '
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RefreshToolStripMenuItem, Me.ChangeToolStripMenuItem})
         Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip2.Name = "MenuStrip2"
-        Me.MenuStrip2.Size = New System.Drawing.Size(395, 24)
+        Me.MenuStrip2.Size = New System.Drawing.Size(268, 24)
         Me.MenuStrip2.TabIndex = 1
         Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1191, 555)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'txtDBpwd
+        '
+        Me.txtDBpwd.Location = New System.Drawing.Point(85, 163)
+        Me.txtDBpwd.Name = "txtDBpwd"
+        Me.txtDBpwd.Size = New System.Drawing.Size(172, 20)
+        Me.txtDBpwd.TabIndex = 11
+        Me.txtDBpwd.UseSystemPasswordChar = True
+        '
+        'RefreshToolStripMenuItem
+        '
+        Me.RefreshToolStripMenuItem.Name = "RefreshToolStripMenuItem"
+        Me.RefreshToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
+        Me.RefreshToolStripMenuItem.Text = "Refresh"
+        '
+        'ChangeToolStripMenuItem
+        '
+        Me.ChangeToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ChangeToolStripMenuItem.Name = "ChangeToolStripMenuItem"
+        Me.ChangeToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ChangeToolStripMenuItem.Text = "Change"
         '
         'frmAdmin
         '
@@ -154,6 +228,8 @@ Partial Class frmAdmin
         Me.SplitContainer2.Panel1.PerformLayout()
         CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer2.ResumeLayout(False)
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -165,7 +241,14 @@ Partial Class frmAdmin
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents SplitContainer1 As SplitContainer
     Friend WithEvents SplitContainer2 As SplitContainer
-    Friend WithEvents dbNfo As ListBox
     Friend WithEvents MenuStrip2 As MenuStrip
     Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents lblDBNfo As Label
+    Friend WithEvents txtPort As TextBox
+    Friend WithEvents txtDBUser As TextBox
+    Friend WithEvents txtDB As TextBox
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents txtDBpwd As TextBox
+    Friend WithEvents RefreshToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChangeToolStripMenuItem As ToolStripMenuItem
 End Class
--- OOPSA/OOPSA/frmAdmin.vb
+++ OOPSA/OOPSA/frmAdmin.vb
@@ -1,15 +1,17 @@
 ﻿Public Class frmAdmin
     Private Sub frmAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        With dbNfo.Items
-            .Add("Server: " & My.Settings.server)
-            .Add("Database: " & My.Settings.database)
-            .Add("Port: " & My.Settings.port)
-            .Add("Username: " & My.Settings.user)
-            .Add("Overview:")
-            .Add(My.Settings.server & ":" & My.Settings.port & " // " & My.Settings.database)
-            .Add(port)
 
-            'Sett opp mulighet for å se database passord på server, trenger admin passordet for sikkerhetssjekk. Typ, trykk en knapp i menustrip, promt adminpw, vis ny nfo
-        End With
+        txtServer.Enabled = 0
+        txtDB.Enabled = 0
+        txtPort.Enabled = 0
+        txtDBUser.Enabled = 0
+        txtDBpwd.Enabled = 0
+
+        txtServer.Text = My.Settings.server
+        txtDB.Text = My.Settings.database
+        txtPort.Text = My.Settings.port
+        txtDBUser.Text = My.Settings.user
+        txtDBpwd.Text = My.Settings.pass
+
     End Sub
 End Class
