c7130f9868696ef6ccbb294d009ed732f65e1155
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-03-07 23:47:30 +0100

    lol

--- OOPSA/OOPSA/frmMetroLogin.Designer.vb
+++ OOPSA/OOPSA/frmMetroLogin.Designer.vb
@@ -29,8 +29,18 @@ Partial Class frmMetroLogin
         Me.btnLgInn = New Syncfusion.Windows.Forms.ButtonAdv()
         Me.aLblUsr = New Syncfusion.Windows.Forms.Tools.AutoLabel()
         Me.aLblPw = New Syncfusion.Windows.Forms.Tools.AutoLabel()
+        Me.Carousel1 = New Syncfusion.Windows.Forms.Tools.Carousel()
+        Me.CarouselItem1 = New Syncfusion.Windows.Forms.Tools.CarouselItem()
+        Me.CarouselItem2 = New Syncfusion.Windows.Forms.Tools.CarouselItem()
+        Me.CarouselItem3 = New Syncfusion.Windows.Forms.Tools.CarouselItem()
+        Me.CarouselItem4 = New Syncfusion.Windows.Forms.Tools.CarouselItem()
         CType(Me.txtUsr, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.txtPw, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Carousel1.SuspendLayout()
+        CType(Me.CarouselItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CarouselItem2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CarouselItem3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CarouselItem4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtUsr
@@ -93,6 +103,79 @@ Partial Class frmMetroLogin
         Me.aLblPw.TabIndex = 5
         Me.aLblPw.Text = "Passord:"
         '
+        'Carousel1
+        '
+        Me.Carousel1.Controls.Add(Me.CarouselItem1)
+        Me.Carousel1.Controls.Add(Me.CarouselItem2)
+        Me.Carousel1.Controls.Add(Me.CarouselItem3)
+        Me.Carousel1.Controls.Add(Me.CarouselItem4)
+        Me.Carousel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Carousel1.HighlightColor = System.Drawing.Color.White
+        Me.Carousel1.ImageshadeColor = System.Drawing.Color.Black
+        Me.Carousel1.ImageSlides = False
+        Me.Carousel1.Items.Add(Me.CarouselItem1)
+        Me.Carousel1.Items.Add(Me.CarouselItem2)
+        Me.Carousel1.Items.Add(Me.CarouselItem3)
+        Me.Carousel1.Items.Add(Me.CarouselItem4)
+        Me.Carousel1.Location = New System.Drawing.Point(236, 0)
+        Me.Carousel1.Name = "Carousel1"
+        Me.Carousel1.PadX = 0
+        Me.Carousel1.PadY = 0
+        Me.Carousel1.Perspective = 2.0!
+        Me.Carousel1.RotateAlways = True
+        Me.Carousel1.ShowImagePreview = False
+        Me.Carousel1.ShowImageShadow = True
+        Me.Carousel1.Size = New System.Drawing.Size(284, 119)
+        Me.Carousel1.TabIndex = 6
+        Me.Carousel1.Text = "Carousel1"
+        Me.Carousel1.TouchTransitionSpeed = 1.0!
+        Me.Carousel1.UseOriginalImageinPreview = False
+        Me.Carousel1.VisualStyle = Syncfusion.Windows.Forms.Tools.CarouselVisualStyle.Metro
+        '
+        'CarouselItem1
+        '
+        Me.CarouselItem1.BackColor = System.Drawing.Color.Black
+        Me.CarouselItem1.Location = New System.Drawing.Point(111, 4)
+        Me.CarouselItem1.Name = "CarouselItem1"
+        Me.CarouselItem1.Size = New System.Drawing.Size(100, 50)
+        Me.CarouselItem1.TabIndex = 0
+        Me.CarouselItem1.TabStop = False
+        Me.CarouselItem1.Tag = 0R
+        Me.CarouselItem1.Text = "CarouselItem1"
+        '
+        'CarouselItem2
+        '
+        Me.CarouselItem2.BackColor = System.Drawing.Color.Silver
+        Me.CarouselItem2.Location = New System.Drawing.Point(200, 18)
+        Me.CarouselItem2.Name = "CarouselItem2"
+        Me.CarouselItem2.Size = New System.Drawing.Size(100, 50)
+        Me.CarouselItem2.TabIndex = 1
+        Me.CarouselItem2.TabStop = False
+        Me.CarouselItem2.Tag = 90.0R
+        Me.CarouselItem2.Text = "CarouselItem2"
+        '
+        'CarouselItem3
+        '
+        Me.CarouselItem3.BackColor = System.Drawing.Color.Maroon
+        Me.CarouselItem3.Location = New System.Drawing.Point(92, 33)
+        Me.CarouselItem3.Name = "CarouselItem3"
+        Me.CarouselItem3.Size = New System.Drawing.Size(100, 50)
+        Me.CarouselItem3.TabIndex = 2
+        Me.CarouselItem3.TabStop = False
+        Me.CarouselItem3.Tag = 180.0R
+        Me.CarouselItem3.Text = "CarouselItem3"
+        '
+        'CarouselItem4
+        '
+        Me.CarouselItem4.BackColor = System.Drawing.Color.Bisque
+        Me.CarouselItem4.Location = New System.Drawing.Point(2, 18)
+        Me.CarouselItem4.Name = "CarouselItem4"
+        Me.CarouselItem4.Size = New System.Drawing.Size(100, 50)
+        Me.CarouselItem4.TabIndex = 3
+        Me.CarouselItem4.TabStop = False
+        Me.CarouselItem4.Tag = 270.0R
+        Me.CarouselItem4.Text = "CarouselItem4"
+        '
         'frmMetroLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -106,6 +189,7 @@ Partial Class frmMetroLogin
         CaptionImage1.Size = New System.Drawing.Size(48, 48)
         Me.CaptionImages.Add(CaptionImage1)
         Me.ClientSize = New System.Drawing.Size(520, 119)
+        Me.Controls.Add(Me.Carousel1)
         Me.Controls.Add(Me.aLblPw)
         Me.Controls.Add(Me.aLblUsr)
         Me.Controls.Add(Me.btnLgInn)
@@ -120,6 +204,11 @@ Partial Class frmMetroLogin
         Me.Text = "TurCare AS Logg Inn | OOPSA BADR08 - Bits Please"
         CType(Me.txtUsr, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.txtPw, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Carousel1.ResumeLayout(False)
+        CType(Me.CarouselItem1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CarouselItem2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CarouselItem3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CarouselItem4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -129,4 +218,9 @@ Partial Class frmMetroLogin
     Friend WithEvents btnLgInn As Syncfusion.Windows.Forms.ButtonAdv
     Friend WithEvents aLblUsr As Syncfusion.Windows.Forms.Tools.AutoLabel
     Friend WithEvents aLblPw As Syncfusion.Windows.Forms.Tools.AutoLabel
+    Friend WithEvents Carousel1 As Syncfusion.Windows.Forms.Tools.Carousel
+    Friend WithEvents CarouselItem1 As Syncfusion.Windows.Forms.Tools.CarouselItem
+    Friend WithEvents CarouselItem2 As Syncfusion.Windows.Forms.Tools.CarouselItem
+    Friend WithEvents CarouselItem3 As Syncfusion.Windows.Forms.Tools.CarouselItem
+    Friend WithEvents CarouselItem4 As Syncfusion.Windows.Forms.Tools.CarouselItem
 End Class
