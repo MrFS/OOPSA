49c9894f1132dd84c0ea8da8b39e8c8ab0b40fd4
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-03-12 00:07:08 +0100

    updatershait

---
+++ OOPSA/OOPSA/classes/Functions/versionControler.vb
@@ -0,0 +1,6 @@
+﻿Public Class versionControler
+    Public Shared major As Integer = 0
+    Public Shared minor As Integer = 2
+    Public Shared build As Integer = 7
+    Public Shared revision As Integer = 4
+End Class
---
+++ OOPSA/chkUpdate/updateModule.vb
@@ -0,0 +1,16 @@
+﻿Imports OOPSA.versionControler
+
+
+Module updateModule
+
+
+
+    Sub Main()
+
+        Console.WriteLine("OOPSA Updater")
+
+        Console.WriteLine("Current version: " & major & "." & minor & "." & build & "." & revision)
+
+    End Sub
+
+End Module
