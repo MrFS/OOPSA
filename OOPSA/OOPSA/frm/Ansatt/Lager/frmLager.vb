b90026d7eccd245814bfc21788c554a47c815a97
Stig Kristian Rothaug <stigkr@student.hist.no>
2016-04-13 15:16:28 +0200

    Endringa intensifies

--- OOPSA/OOPSA/frm/Ansatt/Lager/frmLager.vb
+++ OOPSA/OOPSA/frm/Ansatt/Lager/frmLager.vb
@@ -3,22 +3,24 @@ Imports MySql.Data.MySqlClient
 
 Public Class frmLager
     Inherits MetroForm
-    'Public lager As Integer = 0
 
+    Dim lager As Integer
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ' Dim da = New MySqlCommand()
         Dim sql As New SQL
-        Dim lager As Integer
 
-        Select Case ComboBox3.Text
+
+        Select Case VareEndre.Text Or lagerEndre.Text
             Case "Trondheim"
                 lager = 2
             Case "Bryne"
                 lager = 1
+            Case Else
+                MsgBox("FEIL")
         End Select
 
-        sql.sporring("INSERT INTO Produkt(Produkt_navn, Pris, p_antall, Lager_id) VALUES('" & TextBoxExt7.Text & "', '" & TextBoxExt6.Text & "', '" & TextBoxExt9.Text & "', '" & lager & "')")
+        sql.sporring("INSERT INTO Produkt(Produkt_navn, Pris, p_antall, Lager_id) VALUES('" & NavnLeggTil.Text & "', '" & PrisLeggTil.Text & "', '" & AntLeggTil.Text & "', '" & lager & "')")
 
 
 
@@ -27,9 +29,6 @@ Public Class frmLager
     Private Sub ButtonAdv2_Click(sender As Object, e As EventArgs) Handles ButtonAdv2.Click
         Dim sql As New MySqlCommand("DELETE FROM Produkt WHERE Produkt_navn ='" & ComboBox2.SelectedItem & "'", con)
         sql.ExecuteNonQuery()
-    End Sub
-
-    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
     End Sub
 
     Private Sub frmLager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -47,14 +46,15 @@ Public Class frmLager
         adapter.Fill(ds, "Produkt")
         dt = ds.Tables(0)
         For Each dr In dt.Rows
-            ComboBox3.Items.Add(dr("Produkt_navn"))
+            VareEndre.Items.Add(dr("Produkt_navn"))
             ComboBox2.Items.Add(dr("Produkt_navn"))
         Next
 
 
-        ComboBox1.Items.Add("Trondheim")
-        ComboBox1.Items.Add("Bryne")
-
+        LagerLeggTil.Items.Add("Trondheim")
+        LagerLeggTil.Items.Add("Bryne")
+        lagerEndre.Items.Add("Trondheim")
+        lagerEndre.Items.Add("Bryne")
         'If ComboBox1.Text = "Bryne" Then
         '    lager = 1
         'ElseIf ComboBox1.Text = "Trondheim" Then
@@ -65,10 +65,35 @@ Public Class frmLager
             Initialize()
 
         Catch ex As Exception
+
         End Try
     End Sub
 
-    Private Sub ButtonAdv1_Click(sender As Object, e As EventArgs) Handles ButtonAdv1.Click
+    Private Sub ButtonAdv1_Click(sender As Object, e As EventArgs) Handles EndreKnapp.Click
+
+
+    End Sub
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles VareEndre.SelectedIndexChanged
+        Dim sql As New SQL
+
+
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM Produkt", con)
+        Dim ds = New DataSet
+        Dim dr As DataRow
+        Dim dt As DataTable
+        adapter.Fill(ds, "Produkt")
+        dt = ds.Tables(0)
+        For Each dr In dt.Rows
+            BtnNavnEndre.Text = (dr("Produkt_navn"))
+            btnAntEndre.Text = (dr("p_antall"))
+            BtnPrisEndre.Text = (dr("Pris"))
+            lagerEndre.Text = (dr("Lager_id"))
+        Next
+
+
+
+
+        sql.sporring("INSERT INTO Produkt(Produkt_navn, Pris, p_antall, Lager_id) VALUES('" & BtnNavnEndre.Text & "', '" & BtnPrisEndre.Text & "', '" & btnAntEndre.Text & "', '" & lager & "')")
 
     End Sub
 End Class
