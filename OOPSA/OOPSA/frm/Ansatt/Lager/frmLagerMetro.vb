444b183fa1f26e2838554f39a8eefe2389628705
Stig Kristian Rothaug <stigkr@student.hist.no>
2016-04-23 17:15:49 +0200

    Committerinos

--- OOPSA/OOPSA/frm/Ansatt/Lager/frmLagerMetro.vb
+++ OOPSA/OOPSA/frm/Ansatt/Lager/frmLagerMetro.vb
@@ -5,14 +5,11 @@ Public Class frmLagerMetro
     Inherits MetroForm
     Private sql As New SQL
 
-    Dim dt2 As DataTable
-    Dim dr2 As DataRow
+    Dim dt As DataTable 'For å sende inn lager_id utifra navn
+    Dim dr As DataRow 'For å sende inn lager_id utifra navn
 
-    Dim dt As DataTable
-    Dim dr As DataRow
-
-    Dim dt4 As DataTable
-    Dim dr4 As DataRow
+    Dim dt4 As DataTable ' For å hente inn by navn utifra Lager_id
+    Dim dr4 As DataRow ' For å hente inn by navn utifra Lager_id
 
 
 
@@ -32,16 +29,16 @@ Public Class frmLagerMetro
         'TODO: This line of code loads data into the 'Drift8_2016DataSetLageroversiktALLE.LagerRapportALLE' table. You can move, or remove it, as needed.
         Me.LagerRapportALLETableAdapter.Fill(Me.Drift8_2016DataSetLageroversiktALLE.LagerRapportALLE)
 
-        oppdater() ' Fyller comboboxene
+        oppdater() ' Initially fills comboboxes
 
     End Sub
 
     Private Sub btnLeggTil_Click(sender As Object, e As EventArgs) Handles btnLeggTil.Click
 
-        dt2 = sql.sporring("SELECT Lager_id FROM `Lager` WHERE `by` = '" & LagerLeggTil.Text & "'")
-        dr2 = dt2.Rows(0)
+        dt = sql.sporring("SELECT Lager_id FROM `Lager` WHERE `by` = '" & LagerLeggTil.Text & "'") ' Selects ID of the storage city
+        dr = dt.Rows(0)
 
-        sql.sporring("INSERT INTO Produkt(Produkt_navn, Pris, p_antall, Lager_id) VALUES('" & NavnLeggTil.Text & "', '" & PrisLeggTil.Text & "', '" & AntLeggTil.Text & "', '" & dr2("Lager_id") & "')")
+        sql.sporring("INSERT INTO Produkt(Produkt_navn, Pris, p_antall, Lager_id) VALUES('" & NavnLeggTil.Text & "', '" & PrisLeggTil.Text & "', '" & AntLeggTil.Text & "', '" & dr("Lager_id") & "')") ' Sql query for adding new product uses city instead of id with the query above
 
         oppdater()
 
@@ -49,23 +46,19 @@ Public Class frmLagerMetro
 
     Private Sub EndreKnapp_Click(sender As Object, e As EventArgs) Handles EndreKnapp.Click
 
-        dt = sql.sporring("SELECT Lager_id FROM `Lager` WHERE `by` = '" & lagerEndre.Text & "'")
+        dt = sql.sporring("SELECT Lager_id FROM `Lager` WHERE `by` = '" & lagerEndre.Text & "'") ' Selects the ID of the storage city
         dr = dt.Rows(0)
 
-        sql.sporring("UPDATE Produkt SET Produkt_navn = '" & navnEndreText.Text & "', Pris = '" & prisEndreText.Text & "', p_antall = '" & antEndreText.Text & "', Lager_id = '" & dr("Lager_id") & "' WHERE Produkt_navn='" & VareEndre.Text & "'")
+        sql.sporring("UPDATE Produkt SET Produkt_navn = '" & navnEndreText.Text & "', Pris = '" & prisEndreText.Text & "', p_antall = '" & antEndreText.Text & "', Lager_id = '" & dr("Lager_id") & "' WHERE Produkt_navn='" & VareEndre.Text & "'") 'Changes product and uses the name with the previous query so user does not have to use numbers
 
-        antEndreText.Clear()
-        navnEndreText.Clear()
-        prisEndreText.Clear()
+
 
         oppdater()
 
     End Sub
 
     Private Sub btnSlett_Click(sender As Object, e As EventArgs) Handles btnSlett.Click
-        sql.sporring("DELETE FROM Produkt WHERE Produkt_navn ='" & vareSlett.SelectedItem & "'")
-
-        GridDataBoundGrid1.Refresh()
+        sql.sporring("DELETE FROM Produkt WHERE Produkt_navn ='" & vareSlett.SelectedItem & "'") ' Sql query for deleting chosen product
 
         oppdater()
 
@@ -75,7 +68,7 @@ Public Class frmLagerMetro
 
 
 
-        Dim adapter3 As New MySqlDataAdapter("SELECT * From Produkt WHERE Produkt_navn = '" & VareEndre.Text & "' ", con)
+        Dim adapter3 As New MySqlDataAdapter("SELECT * From Produkt WHERE Produkt_navn = '" & VareEndre.Text & "' ", con) ' Fills textboxes with information the product is in the database with, uses the name of the product to fetch all the information needed.
         Dim ds3 = New DataSet
         Dim dr3 As DataRow
         Dim dt3 As DataTable
@@ -88,7 +81,7 @@ Public Class frmLagerMetro
             prisEndreText.Text = (dr3("Pris"))
         Next
 
-        dt4 = sql.sporring("SELECT `by` FROM Lager WHERE Lager_id = '" & dr3("Lager_id") & "' ")
+        dt4 = sql.sporring("SELECT `by` FROM Lager WHERE Lager_id = '" & dr3("Lager_id") & "' ") ' Used to enter City name instead of a integer into the combobox for changing storage location
         dr4 = dt4.Rows(0)
         lagerEndre.Text = (dr4("by"))
 
@@ -96,9 +89,22 @@ Public Class frmLagerMetro
 
     End Sub
 
-    Private Sub oppdater()
+    Private Sub oppdater() ' Procedure for updating and clearing upon submission of information
 
-        Dim adapter2 As New MySqlDataAdapter("SELECT * FROM Lager", con)
+        NavnLeggTil.Clear()
+        AntLeggTil.Clear()
+        PrisLeggTil.Clear()
+        LagerLeggTil.Items.Clear()
+
+        antEndreText.Clear()
+        navnEndreText.Clear()
+        prisEndreText.Clear()
+        VareEndre.Items.Clear()
+        lagerEndre.Items.Clear()
+
+        vareSlett.Items.Clear()
+
+        Dim adapter2 As New MySqlDataAdapter("SELECT * FROM Lager", con)  ' Fills comboboxes with city names
         Dim ds2 = New DataSet
         Dim dr2 As DataRow
         Dim dt2 As DataTable
@@ -109,7 +115,7 @@ Public Class frmLagerMetro
             lagerEndre.Items.Add(dr2("by"))
         Next
 
-        Dim adapter As New MySqlDataAdapter("SELECT * FROM Produkt", con)
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM Produkt", con) ' Fills comboboxes with product names
         Dim ds = New DataSet
         Dim dr As DataRow
         Dim dt As DataTable
@@ -120,6 +126,9 @@ Public Class frmLagerMetro
             vareSlett.Items.Add(dr("Produkt_navn"))
         Next
 
+        GridDataBoundGrid1.Refresh() ' Updates datagrids
+        GridDataBoundGrid2.Refresh()
+        GridDataBoundGrid3.Refresh()
     End Sub
 
     Private Sub SplitContainer4_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer4.Panel1.Paint
